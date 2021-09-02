14ce56dc089f7d5fc9e1d99409d187fcb50ecf63
Eser Karaceper <52802861+esero88@users.noreply.github.com>
2021-09-02 10:19:23 +0300

    Create Factorial.py
    Simple factorial calculation code block

---
+++ Factorial.py
@@ -0,0 +1,22 @@
+print("""
+***************************
+Factorial Calculation
+
+Press 'q' for exit.
+***************************
+""")
+
+while True:
+    number = input("Number:")
+    if (number == "q"):
+        print("Bye Bye.")
+        break
+
+    else:
+        number = int(number)
+
+        factorial = 1
+
+        for i in range(2,number+1):
+            factorial *= i
+        print("Faktoriyel:",factorial)
