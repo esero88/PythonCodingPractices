1d0fb517fad12de5ae6b2ea9ab46751b80a18dbd
Eser Karaceper <52802861+esero88@users.noreply.github.com>
2021-09-08 10:51:18 +0300

    Create  The.Multiplication.Table.py
    "The Multiplication Table" calculation code

---
+++ The.Multiplication.Table.py
@@ -0,0 +1,9 @@
+print("""
+*****************************
+The Multiplication Table
+*****************************
+""")
+
+for i in range(1,11):
+     for j in range(1,11):
+         print("{} X {} = {}".format(i,j,i*j))
